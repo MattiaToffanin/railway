6bf80e525700005d7e202eb579be10e766ea782d
Toffanin Mattia <mattia.toffanin.1@studenti.unipd.it>
2021-01-12 22:21:02 +0100

    leave train restituisce il treno che era parcheggiato

--- main.cpp
+++ main.cpp
@@ -1,35 +1,21 @@
-#include <iostream>
-#include "Train.h"
 #include "Station.h"
+#include "Train.h"
+#include "time_table.h"
+
+#include "vector"
+#include <iostream>
+#include<fstream>
+#include "string"
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
-
 int main() {
-    Local_station s("Roma", 1234, 2);
-    s.print();
-    cout << endl;
 
-    RegionalTrain t1{1, true};
-    RegionalTrain t2{2, true};
-    RegionalTrain t3{3, true};
-    HighSpeedTrain t4{4, true};
-    HighSpeedTrain t5{5, true};
-    HighSpeedTrain t6{6, true};
+    time_table *t = new time_table();
+    t->avvia_Simulazione();
 
-    s.stop_train(&t1);
-    s.stop_train(&t2);
-    s.stop_train(&t3);
-    s.stop_train(&t4);
-    s.stop_train(&t5);
-    s.stop_train(&t6);
 
-    s.print();
-    cout << endl;
-
-    s.leave_train(0);
-    s.leave_train(4);
-
-    s.print();
     return 0;
 }
---
+++ time_table.h
@@ -0,0 +1,55 @@
+//
+// Creato e modificato da Tommaso Bicego, 1190444
+//
+
+#ifndef ASSEGNAMENTO2_0_TIME_TABLE_H
+#define ASSEGNAMENTO2_0_TIME_TABLE_H
+
+#include "Station.h"
+#include "Train.h"
+
+#include "vector"
+#include <iostream>
+#include<fstream>
+#include "string"
+
+using namespace std;
+
+class time_table {
+public:
+    time_table();
+
+    time_table(const vector<Station *> &listaStazioni);
+
+    void avvia_Simulazione();
+
+
+    //da spostare a private
+    void carica_Tratta();
+
+    void carica_TimeTable();
+
+    vector<string> controlla_Tratta(string text);
+
+    vector<string> controlla_TimeTable(string text);
+
+    void ordina_Tratta();
+
+    vector<string> split(string text);
+
+    vector<int> Orari(vector<string>);
+
+private:
+    vector<Station *> lista_Stazioni;
+    vector<Station *> lista_MainS;
+    vector<Train *> lista_Treni;
+
+
+    //per test
+    void Stampa();
+
+
+};
+
+
+#endif //ASSEGNAMENTO2_0_TIME_TABLE_H
